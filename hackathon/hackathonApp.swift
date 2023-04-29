4899f7fe98087483d6b08263a250a4f3749489f8
Isaac Lopez <isaac.lopez.1b@gmail.com>
2023-04-29 11:01:29 -0600

    Initial Commit

---
+++ hackathon/hackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackathonApp.swift
+//  hackathon
+//
+//  Created by Raymundo Mondragón Lara on 29/04/23.
+//
+
+import SwiftUI
+
+@main
+struct hackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
