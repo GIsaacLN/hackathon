4a7c9ae0f7ac7e5999e1603e1c400cda7ea44197
Ara <ary.castro.dessavre@gmail.com>
2023-04-29 16:24:58 -0600

    Mas cambios del front de iniciocrew

--- hackathon/InicioCrewView.swift
+++ hackathon/InicioCrewView.swift
@@ -8,56 +8,83 @@
 import SwiftUI
 
 struct InicioCrewView: View {
+    
+    @State private var inicio = false
+    @State private var perfil = false
+    @State private var vehiculo = false
+    @State private var mapa = false
+    @State private var ajustes = false
+
+    
     var body: some View {
-        NavigationStack{
+        VStack{
+            
+            Section(){
+                    Text("Empresa")
+                    .font(.system(size: 40, weight: .bold, design: .monospaced))
+            }
+            
+            Spacer()
+            
+            Section(){
+                HStack{
+                    Text("¡Bienvenidos!")
+                        .font(.system(size: 36, weight: .bold, design: .monospaced))
+                    
+                    Spacer()
+                }
+            }
+            
+            NavigationStack{
             Text("HEllos")
                 .toolbar {
                     ToolbarItemGroup(placement: .bottomBar) {
-                            Button(action:  {
-                                    print("Pressed")
-                                }) {
-                                Image(systemName: "person.circle.fill")
-                                    .foregroundColor(.black)
-                            }
-                        
-                        Spacer()
-                            
-                            Button(action:  {
-                                    print("Pressed")
-                                }) {
-                                Image(systemName: "car.fill")
-                                    .foregroundColor(.black)
-                            }
+                        Button(action:  {
+                            print("Pressed")
+                        }) {
+                            Image(systemName: "person.circle.fill")
+                                .foregroundColor(.black)
+                        }
                         
                         Spacer()
                         
-                            Button(action:  {
-                                    print("Pressed")
-                                }) {
-                                Image(systemName: "house")
-                                    .resizable()
-                                    .foregroundColor(.black)
-                                    .frame(width: 50, height: 40)
-                            }
-                        Spacer()
-                        
-                            Button(action:  {
-                                    print("Pressed")
-                                }) {
-                                Image(systemName: "map.fill")
-                                    .foregroundColor(.black)
-                            }
+                        Button(action:  {
+                            print("Pressed")
+                        }) {
+                            Image(systemName: "car.fill")
+                                .foregroundColor(.black)
+                        }
                         
                         Spacer()
                         
-                            Button(action:  {
-                                    print("Pressed")
-                            }) {
-                                Image(systemName: "gear")
-                                    .foregroundColor(.black)
-                            }
+                        Button(action:  {
+                            print("Pressed")
+                        }) {
+                            Image(systemName: "house")
+                                .resizable()
+                                .foregroundColor(.black)
+                                .frame(width: 50, height: 40)
+                        }
+                        Spacer()
+                        
+                        Button(action:  {
+                            print("Pressed")
+                        }) {
+                            Image(systemName: "map.fill")
+                                .foregroundColor(.black)
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            print("Pressed")
+                        }) {
+                            Image(systemName: "gear")
+                                .foregroundColor(.black)
+                        }
+                    }
                 }
-            }
+        }
         }
     }
 }
