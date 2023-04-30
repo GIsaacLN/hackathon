3448f4447c6ea05ab79c9287afc53bca1a83a998
Isaac Lopez <isaac.lopez.1b@gmail.com>
2023-04-30 10:15:59 -0600

    inicio de sesión correcto

--- hackathon/InicioCrewView.swift
+++ hackathon/InicioCrewView.swift
@@ -6,102 +6,123 @@
 //
 
 import SwiftUI
+import FirebaseAuth //Para Cerrar Sesión
+
 
 struct InicioCrewView: View {
-    
+    @EnvironmentObject var userAuth: UserAuth //Para Cerrar Sesión
     @State private var inicio = false
     @State private var perfil = false
     @State private var vehiculo = false
     @State private var mapa = false
     @State private var ajustes = false
     @State private var progress = 0.8
-    let costumFont = Font.custom("against_regular", size: 30)
-    
+
     var body: some View {
-        VStack{
-            
-            Section(){
-                    Text("Empresa")
-                    .font(costumFont)
-            }
-            
-            Spacer()
-            
-            Section(){
-                HStack{
-                    Text("¡Bienvenidos!")
-                        .font(.title)
+        NavigationView {
+            VStack {
+
+        //        Button(action: signOut) { //Para Cerrar Sesión
+        //            Text("Cerrar sesión")
+        //                .foregroundColor(.white)
+        //                .padding()
+        //                .background(Color.red)
+        //                .cornerRadius(8)
+        //        }
+        //        .padding(.top)
+
+
+                Section(){
+                        Text("Empresa")
                 }
-            Spacer()
-            Spacer()
-            Spacer()
-            Spacer()
-            Text("Resumen del viaje")
-                HStack{
-                    Spacer()
-                    Image(systemName: "box.truck")
-                        .resizable()
-                        .frame(width:30,height:30)
-                    ProgressView(value:progress)
-                    Image(systemName: "airplane")
+                
+                Spacer()
+                
+                Section(){
+                    HStack{
+                        Text("¡Bienvenidos!")
+                            .font(.title)
+                    }
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Text("Resumen del viaje")
+                    HStack{
+                        Spacer()
+                        Image(systemName: "box.truck")
+                            .resizable()
+                            .frame(width:30,height:30)
+                        ProgressView(value:progress)
+                        Image(systemName: "airplane")
+                        
+                    }
                     
                 }
                 
-            }
-            
-            
-            NavigationStack{
-                Text("")
-                .toolbar {
-                    ToolbarItemGroup(placement: .bottomBar) {
-                        Button(action:  {
-                            print("Pressed")
-                        }) {
-                            Image(systemName: "person.circle.fill")
-                                .foregroundColor(.black)
+                
+                NavigationStack{
+                    Text("")
+                        .toolbar {
+                            ToolbarItemGroup(placement: .bottomBar) {
+                                Button(action:  {
+                                    print("Pressed")
+                                }) {
+                                    Image(systemName: "person.circle.fill")
+                                        .foregroundColor(.black)
+                                }
+                                
+                                Spacer()
+                                
+                                Button(action:  {
+                                    print("Pressed")
+                                }) {
+                                    Image(systemName: "car.fill")
+                                        .foregroundColor(.black)
+                                }
+                                
+                                Spacer()
+                                
+                                Button(action:  {
+                                    print("Pressed")
+                                }) {
+                                    Image(systemName: "house")
+                                        .resizable()
+                                        .foregroundColor(.black)
+                                        .frame(width: 50, height: 40)
+                                }
+                                Spacer()
+                                
+                                Button(action:  {
+                                    print("Pressed")
+                                }) {
+                                    Image(systemName: "map.fill")
+                                        .foregroundColor(.black)
+                                }
+                                
+                                Spacer()
+                                
+                                Button(action: {
+                                    print("Pressed")
+                                }) {
+                                    Image(systemName: "gear")
+                                        .foregroundColor(.black)
+                                }
+                            }
                         }
-                        
-                        Spacer()
-                        
-                        Button(action:  {
-                            print("Pressed")
-                        }) {
-                            Image(systemName: "car.fill")
-                                .foregroundColor(.black)
-                        }
-                        
-                        Spacer()
-                        
-                        Button(action:  {
-                            print("Pressed")
-                        }) {
-                            Image(systemName: "house")
-                                .resizable()
-                                .foregroundColor(.black)
-                                .frame(width: 50, height: 40)
-                        }
-                        Spacer()
-                        
-                        Button(action:  {
-                            print("Pressed")
-                        }) {
-                            Image(systemName: "map.fill")
-                                .foregroundColor(.black)
-                        }
-                        
-                        Spacer()
-                        
-                        Button(action: {
-                            print("Pressed")
-                        }) {
-                            Image(systemName: "gear")
-                                .foregroundColor(.black)
-                        }
-                    }
                 }
         }
         }
     }
+    
+    //  func signOut() {
+    //      do {
+    //          try Auth.auth().signOut()
+    //          userAuth.isSignedIn = false
+    //      } catch let signOutError as NSError {
+    //          print("Error al cerrar sesión: %@", signOutError)
+    //      }
+    //  }
 }
 
 
--- hackathon/SignUpView.swift
+++ hackathon/SignUpView.swift
@@ -15,6 +15,8 @@ struct NewUser: Codable {
     let apellidos: String
     let correo: String
     let numeroDeTelefono: String
+    let numeroDeEmpleado: String
+    let rfc: String
     let esTransportista: Bool
 }
 
@@ -24,22 +26,19 @@ struct SignUpView: View {
     @State private var name: String = ""
     @State private var lastname: String = ""
     @State private var number: String = ""
+    @State private var employeeNumber: String = ""
+    @State private var rfc: String = ""
     @State private var errorMessage: String = ""
     @State private var isSignedUp = false
     let db = Firestore.firestore()
-
+    let isTransportista: Bool
+    
     var body: some View {
         NavigationLink(destination: SignInView(), isActive: $isSignedUp) {
             EmptyView()
         }
         
         VStack {
-            TextField("Email", text: $email)
-                .keyboardType(.emailAddress)
-                .autocapitalization(.none)
-                .padding()
-                .background(Color.gray.opacity(0.2))
-                .cornerRadius(8)
             TextField("Nombre(s)", text: $name)
                 .keyboardType(.emailAddress)
                 .autocapitalization(.none)
@@ -52,14 +51,35 @@ struct SignUpView: View {
                 .padding()
                 .background(Color.gray.opacity(0.2))
                 .cornerRadius(8)
-
+            TextField("Numero de Telefono", text: $number)
+                .keyboardType(.numberPad)
+                .autocapitalization(.none)
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+            TextField("Numero de Empleado", text: $employeeNumber)
+                .keyboardType(.numberPad)
+                .autocapitalization(.none)
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+            TextField("RFC", text: $rfc)
+                .keyboardType(.emailAddress)
+                .autocapitalization(.none)
+                .padding()
+                .background(Color.gray.opacity(0.2))
+            TextField("Email", text: $email)
+                .keyboardType(.emailAddress)
+                .autocapitalization(.none)
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
             SecureField("Password", text: $password)
                 .padding()
                 .background(Color.gray.opacity(0.2))
                 .cornerRadius(8)
-
             Button(action: signUp) {
-                Text("Sign Up")
+                Text("Registrarse")
                     .foregroundColor(.white)
                     .frame(maxWidth: .infinity)
                     .padding()
@@ -88,7 +108,7 @@ struct SignUpView: View {
     }
 
     func createNewUser() {
-        let newUser = NewUser(nombre: name, apellidos: lastname, correo: email, numeroDeTelefono: number, esTransportista: false)
+        let newUser = NewUser(nombre: name, apellidos: lastname, correo: email, numeroDeTelefono: number,numeroDeEmpleado: employeeNumber,rfc: rfc, esTransportista: isTransportista)
         guard let userId = Auth.auth().currentUser?.uid else {
             print("No se pudo obtener el ID del usuario")
             return
@@ -100,8 +120,8 @@ struct SignUpView: View {
             "correo": newUser.correo,
             "numeroDeTelefono": newUser.numeroDeTelefono,
             "esTransportista": newUser.esTransportista,
-            "numeroDeEmpleado": 0, // Agrega un valor predeterminado para el número de empleado
-            "rfc": "" // Agrega un valor predeterminado para el RFC
+            "numeroDeEmpleado": newUser.numeroDeEmpleado,
+            "rfc": newUser.rfc
         ]) { error in
             if let error = error {
                 print("Error al guardar el usuario: \(error.localizedDescription)")
@@ -116,6 +136,6 @@ struct SignUpView: View {
 
 struct SignUpView_Previews: PreviewProvider {
     static var previews: some View {
-        SignUpView()
+        SignUpView(isTransportista: false)
     }
 }
