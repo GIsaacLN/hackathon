9dc5c1e3480a5a778ac6c1cf8f1c9c39c73ba293
Ara <ary.castro.dessavre@gmail.com>
2023-04-30 11:25:16 -0600

    ya jala

--- hackathon/InicioCrewView.swift
+++ hackathon/InicioCrewView.swift
@@ -17,100 +17,103 @@ struct InicioCrewView: View {
     @State private var mapa = false
     @State private var ajustes = false
     @State private var progress = 0.8
-
+    
     var body: some View {
         NavigationView {
             VStack {
-
-        //        Button(action: signOut) { //Para Cerrar Sesión
-        //            Text("Cerrar sesión")
-        //                .foregroundColor(.white)
-        //                .padding()
-        //                .background(Color.red)
-        //                .cornerRadius(8)
-        //        }
-        //        .padding(.top)
-
-
-                Section(){
-                        Text("Empresa")
-            }
-            
-            Spacer()
-            
-            Section(){
-                HStack{
-                    Text("¡Bienvenidos!")
-                        .font(.title)
-                }
-            Spacer()
-            Spacer()
-            Spacer()
-            Spacer()
-            Text("Resumen del viaje")
-                HStack{
-                    Spacer()
-                    Image(systemName: "box.truck")
-                        .resizable()
-                        .frame(width:30,height:30)
-                    ProgressView(value:progress)
-                    Image(systemName: "airplane")
-                                    
                 
-                NavigationStack{
-                    Text("")
-                        .toolbar {
-                            ToolbarItemGroup(placement: .bottomBar) {
-                                Button(action:  {
-                                    print("Pressed")
-                                }) {
-                                    Image(systemName: "person.circle.fill")
-                                        .foregroundColor(.black)
-                                }
-                                
-                                Spacer()
-                                
-                                Button(action:  {
-                                    print("Pressed")
-                                }) {
-                                    Image(systemName: "car.fill")
-                                        .foregroundColor(.black)
-                                }
-                                
-                                Spacer()
-                                
-                                Button(action:  {
-                                    print("Pressed")
-                                }) {
-                                    Image(systemName: "house")
-                                        .resizable()
-                                        .foregroundColor(.black)
-                                        .frame(width: 50, height: 40)
-                                }
-                                Spacer()
-                                
-                                Button(action:  {
-                                    print("Pressed")
-                                }) {
-                                    Image(systemName: "map.fill")
-                                        .foregroundColor(.black)
-                                }
-                                
-                                Spacer()
-                                
-                                Button(action: {
-                                    print("Pressed")
-                                }) {
-                                    Image(systemName: "gear")
-                                        .foregroundColor(.black)
-                                }
-                            }
-                        }
+                //        Button(action: signOut) { //Para Cerrar Sesión
+                //            Text("Cerrar sesión")
+                //                .foregroundColor(.white)
+                //                .padding()
+                //                .background(Color.red)
+                //                .cornerRadius(8)
+                //                  }
+                //        .padding(.top)
+                
+                
+                Section(){
+                    Text("Empresa")
                 }
-        }
+                
+                Spacer()
+                
+                Section(){
+                    HStack{
+                        Text("¡Bienvenidos!")
+                            .font(.title)
+                    }
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Text("Resumen del viaje")
+                    HStack{
+                        Spacer()
+                        Image(systemName: "box.truck")
+                            .resizable()
+                            .frame(width:30,height:30)
+                        ProgressView(value:progress)
+                        Image(systemName: "airplane")
+                        
+                        
+                        NavigationStack{
+                            Text("")
+                                .toolbar {
+                                    ToolbarItemGroup(placement: .bottomBar) {
+                                        Button(action:  {
+                                            print("Pressed")
+                                        }) {
+                                            Image(systemName: "person.circle.fill")
+                                                .foregroundColor(.black)
+                                        }
+                                        
+                                        Spacer()
+                                        
+                                        Button(action:  {
+                                            print("Pressed")
+                                        }) {
+                                            Image(systemName: "car.fill")
+                                                .foregroundColor(.black)
+                                        }
+                                        
+                                        Spacer()
+                                        
+                                        Button(action:  {
+                                            print("Pressed")
+                                        }) {
+                                            Image(systemName: "house")
+                                                .resizable()
+                                                .foregroundColor(.black)
+                                                .frame(width: 50, height: 40)
+                                        }
+                                        Spacer()
+                                        
+                                        Button(action:  {
+                                            print("Pressed")
+                                        }) {
+                                            Image(systemName: "map.fill")
+                                                .foregroundColor(.black)
+                                        }
+                                        
+                                        Spacer()
+                                        
+                                        Button(action: {
+                                            print("Pressed")
+                                        }) {
+                                            Image(systemName: "gear")
+                                                .foregroundColor(.black)
+                                        }
+                                    }
+                                }
+                        }
+                    }
+                }
+            }
         }
     }
-    
+}
+
     //  func signOut() {
     //      do {
     //          try Auth.auth().signOut()
@@ -119,7 +122,6 @@ struct InicioCrewView: View {
     //          print("Error al cerrar sesión: %@", signOutError)
     //      }
     //  }
-}
 
 
 struct InicioCrewView_Previews: PreviewProvider {
