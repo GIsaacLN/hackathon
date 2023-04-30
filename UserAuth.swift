03f8e63e039ec4edd5fad9082b7830f2d3f8979f
Isaac Lopez <isaac.lopez.1b@gmail.com>
2023-04-30 10:29:12 -0600

    userAuth

---
+++ UserAuth.swift
@@ -0,0 +1,13 @@
+//
+//  UserAuth.swift
+//  hackathon
+//
+//  Created by Raymundo Mondragón Lara on 30/04/23.
+//
+
+import SwiftUI
+import Combine
+
+class UserAuth: ObservableObject {
+    @Published var isSignedIn = false
+}
