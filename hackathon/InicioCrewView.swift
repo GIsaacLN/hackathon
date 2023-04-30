8d3d61b2170b84a842e491c7a46de681ff470a03
Ara <ary.castro.dessavre@gmail.com>
2023-04-29 20:41:18 -0600

    Un buen de vistas

--- hackathon/InicioCrewView.swift
+++ hackathon/InicioCrewView.swift
@@ -15,14 +15,14 @@ struct InicioCrewView: View {
     @State private var mapa = false
     @State private var ajustes = false
     @State private var progress = 0.8
-
+    let costumFont = Font.custom("against_regular", size: 30)
     
     var body: some View {
         VStack{
             
             Section(){
                     Text("Empresa")
-                    .font(Font.custom("against_regular", size: 30))
+                    .font(costumFont)
             }
             
             Spacer()
